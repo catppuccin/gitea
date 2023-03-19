5607ab9f535569a5fcafe00e1d7d9bed4cff6c18
winston <hey@winston.sh>
2023-03-19 07:34:15 +0100

    build: add nix shell

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {buildInputs = [pkgs.deno];};
+      }
+    );
+}
