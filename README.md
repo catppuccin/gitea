b00c20e7a772185703cd82fb1e1b7a870524991f
winston <hey@winston.sh>
2023-08-22 13:22:46 +0200

    build: use deno, auto-generate README conf snippet
    Closes #10

--- README.md
+++ README.md
@@ -38,7 +38,22 @@
 
 1. Download the [latest release](https://github.com/catppuccin/gitea/releases/latest).
 2. Place the CSS files in your `$GITEA_CUSTOM/public/css` directory.
-3. Add the themes to your [`app.ini`](https://docs.gitea.io/en-us/customizing-gitea/#customizing-the-look-of-gitea).
+3. Add the themes to your `app.ini`. See the [Gitea documentation](https://docs.gitea.io/en-us/customizing-gitea/#customizing-the-look-of-gitea).\
+   Here's a snippet if you want to offer all Flavor-Accent combinations:
+
+<details>
+<summary>app.ini</summary>
+<!-- AUTOGEN:INI START -->
+<!-- the following section is auto-generated, do not edit -->
+
+```
+[ui]
+THEMES = catppuccin-latte-rosewater,catppuccin-latte-flamingo,catppuccin-latte-pink,catppuccin-latte-mauve,catppuccin-latte-red,catppuccin-latte-maroon,catppuccin-latte-peach,catppuccin-latte-yellow,catppuccin-latte-green,catppuccin-latte-teal,catppuccin-latte-sky,catppuccin-latte-sapphire,catppuccin-latte-blue,catppuccin-latte-lavender,catppuccin-frappe-rosewater,catppuccin-frappe-flamingo,catppuccin-frappe-pink,catppuccin-frappe-mauve,catppuccin-frappe-red,catppuccin-frappe-maroon,catppuccin-frappe-peach,catppuccin-frappe-yellow,catppuccin-frappe-green,catppuccin-frappe-teal,catppuccin-frappe-sky,catppuccin-frappe-sapphire,catppuccin-frappe-blue,catppuccin-frappe-lavender,catppuccin-macchiato-rosewater,catppuccin-macchiato-flamingo,catppuccin-macchiato-pink,catppuccin-macchiato-mauve,catppuccin-macchiato-red,catppuccin-macchiato-maroon,catppuccin-macchiato-peach,catppuccin-macchiato-yellow,catppuccin-macchiato-green,catppuccin-macchiato-teal,catppuccin-macchiato-sky,catppuccin-macchiato-sapphire,catppuccin-macchiato-blue,catppuccin-macchiato-lavender,catppuccin-mocha-rosewater,catppuccin-mocha-flamingo,catppuccin-mocha-pink,catppuccin-mocha-mauve,catppuccin-mocha-red,catppuccin-mocha-maroon,catppuccin-mocha-peach,catppuccin-mocha-yellow,catppuccin-mocha-green,catppuccin-mocha-teal,catppuccin-mocha-sky,catppuccin-mocha-sapphire,catppuccin-mocha-blue,catppuccin-mocha-lavender
+```
+
+<!-- AUTOGEN:INI END -->
+</details>
+
 4. Restart your Gitea instance.
 5. Select the theme in Gitea > Account > Settings > Appearance.
 
---
+++ build.ts
@@ -0,0 +1,95 @@
+#!/usr/bin/env -S deno run -A
+import * as path from "std/path";
+import * as sass from "sass";
+import ctp from "npm:@catppuccin/palette";
+
+const builder = (flavor: string, accent: string) => `
+@import "@catppuccin/palette/scss/${flavor}";
+$accent: $${accent};
+$isDark: ${flavor !== "latte"};
+@import "theme";
+`;
+
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+
+const accents = [
+  "rosewater",
+  "flamingo",
+  "pink",
+  "mauve",
+  "red",
+  "maroon",
+  "peach",
+  "yellow",
+  "green",
+  "teal",
+  "sky",
+  "sapphire",
+  "blue",
+  "lavender",
+];
+
+Deno.mkdirSync(path.join(__dirname, "dist"), { recursive: true });
+
+const flavors = Object.keys(ctp.variants);
+for (const flavor of flavors) {
+  for (const accent of accents) {
+    const input = builder(flavor, accent);
+    const result = sass.compileString(input, {
+      loadPaths: [
+        path.join(__dirname, "src"),
+        path.join(__dirname, "node_modules"),
+      ],
+    });
+
+    Deno.writeTextFileSync(
+      path.join(__dirname, "dist", `theme-catppuccin-${flavor}-${accent}.css`),
+      result.css,
+    );
+  }
+}
+
+const updateReadme = ({
+  readme,
+  section,
+  newContent,
+}: {
+  readme: string;
+  section: string;
+  newContent: string;
+}): string => {
+  const preamble =
+    "<!-- the following section is auto-generated, do not edit -->";
+  const startMarker = `<!-- AUTOGEN:${section.toUpperCase()} START -->`;
+  const endMarker = `<!-- AUTOGEN:${section.toUpperCase()} END -->`;
+  const wrapped = `${startMarker}\n${preamble}\n${newContent}\n${endMarker}`;
+
+  if (!(readme.includes(startMarker) && readme.includes(endMarker))) {
+    throw new Error("Markers not found in README.md");
+  }
+
+  const pre = readme.split(startMarker)[0];
+  const end = readme.split(endMarker)[1];
+  return pre + wrapped + end;
+};
+
+const readme = Deno.readTextFileSync(path.join(__dirname, "README.md"));
+const newcontent = updateReadme({
+  readme,
+  section: "ini",
+  newContent: `
+\`\`\`
+[ui]
+THEMES = ${
+    flavors
+      .map((f) =>
+        accents
+          .map((a) => `catppuccin-${f}-${a}`)
+          .join(",")
+      ).join(",")
+  }
+\`\`\`
+`,
+});
+
+Deno.writeTextFileSync(path.join(__dirname, "README.md"), newcontent);
